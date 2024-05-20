b2fe6f379d96ee3a65f07f1378c1c8012b8cbfa8
ibrahim keskin <brhmkskn@outlook.com>
2024-05-20 15:04:58 +0200

    communication between 2 Docker services

--- MicroService01.Api/Controllers/ProductsController.cs
+++ MicroService01.Api/Controllers/ProductsController.cs
@@ -9,9 +9,21 @@ namespace MicroService01.Api.Controllers
     public class ProductsController : ControllerBase
     {
         private readonly AppDbContext _context;
-        public ProductsController(AppDbContext context)
+        private readonly IConfiguration _configuration;
+        public ProductsController(AppDbContext context, IConfiguration configuration)
         {
             _context = context;
+            _configuration = configuration;
+        }
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var httpclient= new HttpClient();
+            var baseUrl = _configuration.GetSection("Microservices")["baseUrlMicroservice2"];
+            var response = await httpclient.GetAsync($"{baseUrl}/api/Products");
+
+            var context=await response.Content.ReadAsStringAsync();
+            return Ok(context);
         }
         [HttpPost]
         public IActionResult Save()
---
+++ MicroService02.Api/Controllers/ProductsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MicroService02.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+
+            return Ok(new { name="Service 2 Pen "+DateTime.Now.Millisecond});
+        }
+    }
+}
