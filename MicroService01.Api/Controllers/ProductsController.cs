a6f82546381fbc6905d4e9d4e2721000a492d1a1
ibrahim keskin <brhmkskn@outlook.com>
2024-05-20 14:24:15 +0200

    add Ef core and save data to sqlserver container.

---
+++ MicroService01.Api/Controllers/ProductsController.cs
@@ -0,0 +1,24 @@
+﻿using MicroService01.Api.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MicroService01.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        public ProductsController(AppDbContext context)
+        {
+            _context = context;
+        }
+        [HttpPost]
+        public IActionResult Save()
+        {
+            _context.Products.Add(new Product { Name = "Pen 1" });
+            _context.SaveChanges();
+            return Ok();
+        }
+    }
+}
---
+++ MicroService01.Api/Models/Product.cs
@@ -0,0 +1,8 @@
+﻿namespace MicroService01.Api.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null;
+    }
+}
